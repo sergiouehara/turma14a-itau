9ede7ac82987e5b462e1867274d0bd02a3283148
sergiouehara <s.hiroshi.uehara@uol.com.br>
2021-02-04 15:15:39 -0300

    alteracao

--- index.php
+++ index.php
@@ -6,12 +6,15 @@
   
 
 <div style="display:flex">
-	<div style="width:200px;height:200px;background:yellow;padding:50px;margin:10px">
-   	Um teste em um quadrado 
+	<div style="width:200px;height:200px;background:yellow;padding:50px;gitmargin:10px">
+   	Um teste em um quadrado
+    <br>
+    <img src="b1.png" style="width: 200px">      
 	</div>
     
 	<div 	 style="width:200px;height:200px;background:blue;color:white;padding:50px;margin:10px">
    	Um teste em um quadrado 
+    <br>
+    <img src="b2.png" style="width: 200px">   
 	</div>
 </div>
-git
