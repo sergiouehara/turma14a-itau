a5304bdd0702fb3b9f34bdec8bc1db9ffc46bf18
sergiouehara <s.hiroshi.uehara@uol.com.br>
2021-02-04 15:47:05 -0300

    alteracao preco

--- index.php
+++ index.php
@@ -6,15 +6,30 @@
   
 
 <div style="display:flex">
-	<div style="width:200px;height:200px;background:yellow;padding:50px;gitmargin:10px">
+	<div style="width:200px;height:400px;background:yellow;padding:50px;gitmargin:10px">
    	Um teste em um quadrado
     <br>
-    <img src="b1.png" style="width: 200px">      
+    <img src="b1git.png" style="width: 200px"> 
+    <br><br>
+    R$ 550,00 
+    <br><br>
+    <a href="https://pag.ae/7WRynf6X1" target="_blank"  
+    style="display:block;background:green;color:white;text-align:center;padding:20px;border-radius:10px">Comparar agora</a><br>
+	<a href="https://api.whatsapp.com/send?phone=55999999999999&text=ol%C3%A1%20vendedor%20quero%20comprar%20de%20vc" target="_blank" >Entrar em contato com o vendedor</a>
+    
 	</div>
     
-	<div 	 style="width:200px;height:200px;background:blue;color:white;padding:50px;margin:10px">
+	<div 	 style="width:200px;height:400px;background:blue;color:white;padding:50px;margin:10px">
    	Um teste em um quadrado 
     <br>
-    <img src="b2.png" style="width: 200px">   
+    <img src="b2.png" style="width: 200px">  
+    <br><br>
+    R$ 850,00
+    <br><br>
+    <a href="https://pag.ae/7WRynf6X1" target="_blank" 
+    style="display:block;background:green;color:white;text-align:center;padding:20px;border-radius:10p<br><br>
+    <a href="https://pag.ae/7WRynf6X1" target="_blank" x">Comparar agora</a><br>
+	<a href="https://api.whatsapp.com/send?phone=55999999999999&text=ol%C3%A1%20vendedor%20quero%20comprar%20de%20vc" target="_blank" style="color:white">Entrar em contato com o vendedor</a>
+
 	</div>
 </div>
